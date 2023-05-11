fd9eeef69b515c268c8c40e18346688671280179
Yehia Mohamed <102627389+YehiaFarghaly@users.noreply.github.com>
2023-05-11 15:37:24 +0200

    add SuppressionCommentFilter to checkstyle

--- src/main/java/sql/SQLTerm.java
+++ src/main/java/sql/SQLTerm.java
@@ -1,14 +1,17 @@
+//CHECKSTYLE:OFF: MemberName
+
 package sql;
 
 public class SQLTerm{
-String strTableName, strColumnName, strOperator;
-Object objValue;
+String _strTableName, _strColumnName, _strOperator;
+Object _objValue;
 
     public SQLTerm(String strTableName, String strColumnName, String strOperator, Object objValue) {
-        this.strTableName = strTableName;
-        this.strColumnName = strColumnName;
-        this.strOperator = strOperator;
-        this.objValue = objValue;
+        this._strTableName = strTableName;
+        this._strColumnName = strColumnName;
+        this._strOperator = strOperator;
+        this._objValue = objValue;
     }
     
 }
+//CHECKSTYLE:ON: MemberName
