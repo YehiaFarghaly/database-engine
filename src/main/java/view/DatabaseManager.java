1fa2c6c319a21a83350064847e17bf0433a02a01
abdulrhman500 <abdulrhmanabotor@gmail.com>
2023-05-14 23:33:08 +0300

    Resolve #67 CheckStyle Done

--- src/main/java/view/DatabaseManager.java
+++ src/main/java/view/DatabaseManager.java
@@ -1,50 +1,37 @@
 package view;
 
-import app.DBApp;
+import constants.Constants;
 import exceptions.DBAppException;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import view.boxes.TitleBox;
+import view.components.AppBorder;
+import view.wrappers.Wrapper;
+import static javafx.application.Application.launch;
 
-public class DatabaseManager  {
-//	TitleBox titleBox = new TitleBox();
-//	Wrapper queryWrapper = new Wrapper(Pos.CENTER);
-//	Wrapper tableWrapper = new Wrapper(Pos.TOP_CENTER);
+public class DatabaseManager {
+	TitleBox titleBox = new TitleBox();
+	Wrapper queryWrapper = new Wrapper(Pos.CENTER);
+	Wrapper tableWrapper = new Wrapper(Pos.TOP_CENTER);
 
-//	public void start(Stage primaryStage) {
-//		AppBorder border = new AppBorder(titleBox, queryWrapper, tableWrapper);
-//		Scene root = new Scene(border, Constants.SCENE_WIDTH, Constants.SCENE_HEIGHT);
-//		primaryStage.setScene(root);
-//		primaryStage.setResizable(false);
-//		primaryStage.show();
-//	}
+	public void start(Stage primaryStage) {
+		AppBorder border = new AppBorder(titleBox, queryWrapper, tableWrapper);
+		Scene root = new Scene(border, Constants.SCENE_WIDTH, Constants.SCENE_HEIGHT);
+		primaryStage.setScene(root);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
 
 	public static void main(String[] args) throws DBAppException {
-//		launch(args);
 
+		launch(args);
 
-		DBApp app = new DBApp();
-		StringBuffer s = new StringBuffer();
-
-		//OR (l=u AnD wejo=vfkm and t=1)
-		//c=oo OR f=1 AND p=0 OR I=2 AND R=09 AND 1=1
-		s.append("INSERT INTO employees (id, name, salary)\n" +
-				"VALUES (1, 'John Doe', 5000);");
-//		s.append("select abc,d,f,t FROM tttt where b=bk or f=0 AND 1=1;");
-//		s.append("DELETE FROM employees WHERE salary < 5000 and a>4;");
-//s.append("CREATE INDEX idx_customers ON customers (last_name, first_name);\n");
-//s.append("CREATE TABLE employees (\n" +
-//		"  employee_id INT PRIMARY KEY," +
-//		"id INT" +
-//		");");
-
-//		s.append("UPDATE employees\n" +
-//				"SET salary = 5000 , l =587" +
-//				"WHERE department = 'IT';");
-//		""; select * from fv;CREATE TABLE employees (\n" +
-//				"    id INT PRIMARY KEY,\n" +
-//				"    name VARCHAR(50),\n" +
-//				"    age INT,\n" +
-//				"    salary DECIMAL(10, 2)\n" +
-//				");");
-		app.parseSQL(s);
+//		DBApp app = new DBApp();
+//		StringBuffer s = new StringBuffer();
+//		s.append("INSERT INTO employees (id, name, salary)\n" +
+//				"VALUES (1, 'John Doe', 5000);");
+//		app.parseSQL(s);
 
 	}
 
