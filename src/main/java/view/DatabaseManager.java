8287b984b0fb7ad5cfeba582e999883fbd945ae7
Yehia Mohamed <102627389+YehiaFarghaly@users.noreply.github.com>
2023-05-16 00:17:57 +0200

    remove testtables

--- src/main/java/view/DatabaseManager.java
+++ src/main/java/view/DatabaseManager.java
@@ -1,14 +1,12 @@
 package view;
 
 import constants.Constants;
-import exceptions.DBAppException;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import view.boxes.TitleBox;
 import view.components.AppBorder;
 import view.wrappers.Wrapper;
-import static javafx.application.Application.launch;
 
 public class DatabaseManager {
 	TitleBox titleBox = new TitleBox();
@@ -23,16 +21,6 @@ public class DatabaseManager {
 		primaryStage.show();
 	}
 
-	public static void main(String[] args) throws DBAppException {
 
-		launch(args);
-
-//		DBApp app = new DBApp();
-//		StringBuffer s = new StringBuffer();
-//		s.append("INSERT INTO employees (id, name, salary)\n" +
-//				"VALUES (1, 'John Doe', 5000);");
-//		app.parseSQL(s);
-
-	}
 
 }
