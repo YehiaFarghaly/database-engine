059e612ce84b4029c4c8cfff047f8d92bbb473b6
Abdelrahman Elmeky <65960126+Aelmeky@users.noreply.github.com>
2023-05-18 17:53:28 +0200

    methods should not be morethan 20 lines

--- src/main/java/util/search/Selector.java
+++ src/main/java/util/search/Selector.java
@@ -11,7 +11,6 @@ import storage.index.*;
 import storage.index.OctreeIndex;
 import util.TypeParser;
 import util.filecontroller.Serializer;
-
 import java.util.*;
 
 public class Selector {
@@ -77,19 +76,22 @@ public class Selector {
 	}
 
 	private static Hashtable<String, Object> getMax(String tableName, String[] col) {
-		Hashtable<String, Object> colMax = new Hashtable<String, Object>();
 		CsvReader reader = new CsvReader();
 		List<String[]> columns = reader.readTable(tableName);
+		return getColAtIdx(columns, Constants.COL_MAX_VALUE_INDEX, col);
+	}
 
+	private static Hashtable<String, Object> getColAtIdx(List<String[]> columns, int idx, String[] colNames) {
+		Hashtable<String, Object> colMax = new Hashtable<String, Object>();
 		for (String[] arr : columns) {
-			if (arr[Constants.COLUMN_NAME_INDEX].equals(col[0])) {
-				colMax.put(col[0],
+			if (arr[idx].equals(colNames[0])) {
+				colMax.put(colNames[0],
 						TypeParser.typeParser(arr[Constants.COL_MAX_VALUE_INDEX], arr[Constants.COLUMN_TYPE_INDEX]));
-			} else if (arr[Constants.COLUMN_NAME_INDEX].equals(col[1])) {
-				colMax.put(col[1],
+			} else if (arr[idx].equals(colNames[1])) {
+				colMax.put(colNames[1],
 						TypeParser.typeParser(arr[Constants.COL_MAX_VALUE_INDEX], arr[Constants.COLUMN_TYPE_INDEX]));
-			} else if (arr[Constants.COLUMN_NAME_INDEX].equals(col[2])) {
-				colMax.put(col[2],
+			} else if (arr[idx].equals(colNames[2])) {
+				colMax.put(colNames[2],
 						TypeParser.typeParser(arr[Constants.COL_MAX_VALUE_INDEX], arr[Constants.COLUMN_TYPE_INDEX]));
 			}
 		}
@@ -97,60 +99,48 @@ public class Selector {
 	}
 
 	private static Hashtable<String, Object> getMin(String tableName, String[] col) {
-		Hashtable<String, Object> colMin = new Hashtable<String, Object>();
 		CsvReader reader = new CsvReader();
 		List<String[]> columns = reader.readTable(tableName);
-
-		for (String[] arr : columns) {
-			if (arr[Constants.COLUMN_NAME_INDEX].equals(col[0])) {
-				colMin.put(col[0],
-						TypeParser.typeParser(arr[Constants.COL_MIN_VALUE_INDEX], arr[Constants.COLUMN_TYPE_INDEX]));
-			} else if (arr[Constants.COLUMN_NAME_INDEX].equals(col[1])) {
-				colMin.put(col[1],
-						TypeParser.typeParser(arr[Constants.COL_MIN_VALUE_INDEX], arr[Constants.COLUMN_TYPE_INDEX]));
-			} else if (arr[Constants.COLUMN_NAME_INDEX].equals(col[2])) {
-				colMin.put(col[2],
-						TypeParser.typeParser(arr[Constants.COL_MIN_VALUE_INDEX], arr[Constants.COLUMN_TYPE_INDEX]));
-			}
-		}
-		return colMin;
+		return getColAtIdx(columns, Constants.COL_MIN_VALUE_INDEX, col);
 	}
 
-	private static List getPageIndcies(String[] strOperator, OctreeIndex index, String[] ColumnsNames,
-			Hashtable<String, Object> colNameValue, String TableName) throws DBAppException {
-		Hashtable<String, Object> min = getMin(TableName, ColumnsNames);
-		Hashtable<String, Object> max = getMax(TableName, ColumnsNames);
-		Object minbounds[] = new Object[3];
-		Object maxbounds[] = new Object[3];
-		int minmask = 0;
-		int maxmask = 0;
+	private static List getPageIndcies(String[] strOperator, OctreeIndex index, String[] columnsNames,
+			Hashtable<String, Object> colNameValue, String tableName) throws DBAppException {
+		Hashtable<String, Object> min = getMin(tableName, columnsNames);
+		Hashtable<String, Object> max = getMax(tableName, columnsNames);
+		Object minBounds[] = new Object[3];
+		Object maxBounds[] = new Object[3];
+		int minMask = 0;
+		int maxMask = 0;
 		for (int i = 0; i < strOperator.length; i++) {
-			if (strOperator[i].equals(Constants.EQUAL)) {
-				minbounds[i] = colNameValue.get(ColumnsNames[i]);
-				maxbounds[i] = colNameValue.get(ColumnsNames[i]);
-			} else if (strOperator[i].equals(Constants.GREATER_THAN_OR_EQUAL)) {
-				minbounds[i] = colNameValue.get(ColumnsNames[i]);
-				maxbounds[i] = max.get(ColumnsNames[i]);
-			} else if (strOperator[i].equals(Constants.GREATER_THAN)) {
-				minbounds[i] = colNameValue.get(ColumnsNames[i]);
-				maxbounds[i] = max.get(ColumnsNames[i]);
-				minmask |= (1 << i);
-			} else if (strOperator[i].equals(Constants.LESS_THAN_OR_EQUAL)) {
-				minbounds[i] = min.get(ColumnsNames[i]);
-				maxbounds[i] = colNameValue.get(ColumnsNames[i]);
-			} else if (strOperator[i].equals(Constants.LESS_THAN)) {
-				minbounds[i] = min.get(ColumnsNames[i]);
-				maxbounds[i] = colNameValue.get(ColumnsNames[i]);
-				maxmask |= (1 << i);
-			} else if (strOperator[i].equals(Constants.LESS_THAN)) {
-				minbounds[i] = min.get(ColumnsNames[i]);
-				maxbounds[i] = max.get(ColumnsNames[i]);
-			}
+			String operator = strOperator[i];
+			minBounds[i] = getMinBound(operator, i, colNameValue, columnsNames, min);
+			maxBounds[i] = getMaxBound(operator, i, colNameValue, columnsNames, max);
+			if (operator.equals(Constants.GREATER_THAN))
+				minMask |= (1 << i);
+			else if (operator.equals(Constants.LESS_THAN))
+				maxMask |= (1 << i);
 		}
+		OctreeBounds bounds = new OctreeBounds(minBounds[0], minBounds[1], minBounds[2], maxBounds[0], maxBounds[1], maxBounds[2]);
+		return index.query(bounds, minMask, maxMask);
+	}
 
-		OctreeBounds bounds = new OctreeBounds(minbounds[0], minbounds[1], minbounds[2], maxbounds[0], maxbounds[1],
-				maxbounds[2]);
-		return index.query(bounds, minmask, maxmask);
+	private static Object getMinBound(String operator, int idx, Hashtable<String, Object> colNameValue,
+			String[] columnNames, Hashtable<String, Object> min) {
+		if (operator.equals(Constants.EQUAL) || operator.equals(Constants.GREATER_THAN)
+				|| operator.equals(Constants.GREATER_THAN_OR_EQUAL))
+			return colNameValue.get(columnNames[idx]);
+		else
+			return min.get(columnNames[idx]);
+	}
+
+	private static Object getMaxBound(String operator, int idx, Hashtable<String, Object> colNameValue,
+			String[] columnNames, Hashtable<String, Object> max) {
+		if (operator.equals(Constants.EQUAL) || operator.equals(Constants.LESS_THAN)
+				|| operator.equals(Constants.LESS_THAN_OR_EQUAL))
+			return colNameValue.get(columnNames[idx]);
+		else
+			return max.get(columnNames[idx]);
 	}
 
 	public static HashSet<Integer> getPagesNumbers(List pagespathes, Table table) {
@@ -160,9 +150,9 @@ public class Selector {
 		}
 		return result;
 	}
-	
-	private static Vector<Vector<Tuple>> getSelectFromTableWithIndex(SQLTerm[] arrSQLTerms, HashSet<Integer> pagenumbers
-			,Hashtable <String, Object> colNameValue) throws DBAppException{
+
+	private static Vector<Vector<Tuple>> getSelectFromTableWithIndex(SQLTerm[] arrSQLTerms,
+			HashSet<Integer> pagenumbers, Hashtable<String, Object> colNameValue) throws DBAppException {
 		Vector<Vector<Tuple>> result = new Vector<>();
 		int idx = 0;
 		for (String key : colNameValue.keySet()) {
@@ -173,15 +163,15 @@ public class Selector {
 		}
 		return result;
 	}
-	
-	private static Hashtable<String, Object> setcolNameValues(SQLTerm[] arrSQLTerms){
+
+	private static Hashtable<String, Object> setcolNameValues(SQLTerm[] arrSQLTerms) {
 		Hashtable<String, Object> colNameValue = new Hashtable<>();
 		for (int i = 0; i < arrSQLTerms.length; i++) {
 			colNameValue.put(arrSQLTerms[i]._strColumnName, arrSQLTerms[i]._objValue);
 		}
 		return colNameValue;
 	}
-	
+
 	private static String[] setstrOperators(SQLTerm[] arrSQLTerms) {
 		String strOperator[] = new String[arrSQLTerms.length];
 		for (int i = 0; i < arrSQLTerms.length; i++) {
@@ -189,7 +179,6 @@ public class Selector {
 		}
 		return strOperator;
 	}
-	
 
 	public static Vector<Tuple> selectWithIndex(OctreeIndex index, SQLTerm[] arrSQLTerms, String[] ColumnsNames,
 			Table table) throws DBAppException {
@@ -201,25 +190,13 @@ public class Selector {
 		Hashtable<String, Object> colNameValue = setcolNameValues(arrSQLTerms);
 		pagepathes = getPageIndcies(strOperator, index, ColumnsNames, colNameValue, table.getName());
 		pagenumbers = getPagesNumbers(pagepathes, table);
-		result.addAll(getSelectFromTableWithIndex( arrSQLTerms, pagenumbers, colNameValue));
+		result.addAll(getSelectFromTableWithIndex(arrSQLTerms, pagenumbers, colNameValue));
 		strarrOperators[0] = Constants.AND_OPERATION;
 		strarrOperators[1] = Constants.AND_OPERATION;
 		return applyArrOperators(result, strarrOperators);
 
 	}
 
-
-
-	
-
-//	private static Vector<Tuple> getResultforIndexSelect(Page page, int idx, SQLTerm[] terms) {
-//		Hashtable<String, Object> colNameVal = new Hashtable<>();
-//		colNameVal.put(terms[idx]._strColumnName, terms[idx]._objValue);
-//		String operator = terms[idx]._strOperator;
-//		return page.select(colNameVal, operator);
-//	}
-
-	
 	public static Vector<Tuple> selectFromTableWithIndex(String strTableName, Hashtable<String, Object> colNameValue,
 			String operator, HashSet<Integer> pageIndex) throws DBAppException {
 		Vector<Tuple> result = new Vector<>();
@@ -255,7 +232,7 @@ public class Selector {
 
 		return result;
 	}
-	
+
 	private static String[] removeFromStrarrOperators(String[] strarrOperators, int index) {
 		String[] res = new String[strarrOperators.length];
 		for (int i = 0; i < strarrOperators.length && i != index; i++) {
@@ -266,7 +243,8 @@ public class Selector {
 		return res;
 	}
 
-	public static Iterator selectWithMoreThanTwoOperators(SQLTerm[] sqlTerms, String[] operators) throws DBAppException {
+	public static Iterator selectWithMoreThanTwoOperators(SQLTerm[] sqlTerms, String[] operators)
+			throws DBAppException {
 		Vector<Vector<Tuple>> result = new Vector<>();
 		int len = sqlTerms.length;
 		for (int i = 0; i < len - 1; i++) {
